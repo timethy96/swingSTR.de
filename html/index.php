05627d7a76cf4a13638d328d393a127211956ad7
timethy96 <timo@bilhoefer.de>
2021-09-18 11:01:27 +0200

    add swingstr-ics calendar

--- html/index.php
+++ html/index.php
@@ -46,7 +46,7 @@ $lang = 'de';
     </div>
 
     <div id="gcal">
-        <iframe src="https://www.google.com/calendar/embed?showTitle=0&wkst=2&bgcolor=%23ffffff&src=kja74l310d5sg6rnpcns38k3fk@group.calendar.google.com&src=kontakt@swing-stuttgart.de&src=swingandcake@gmail.com&src=nqj3q2taqm1aike4i6opvlt37o@group.calendar.google.com&src=a2vipdbb7ghsdk71sfblvivs3k@group.calendar.google.com&color=%235A6986&ctz=Europe%2FBerlin"></iframe>
+        <iframe src="https://www.google.com/calendar/embed?showTitle=0&wkst=2&bgcolor=%23ffffff&src=5eata97e5pmn9tma5c36ff5bqt54kbn3@import.calendar.google.com&src=swingandcake@gmail.com&src=a2vipdbb7ghsdk71sfblvivs3k@group.calendar.google.com&color=%235A6986&ctz=Europe%2FBerlin"></iframe>
     </div>
 
     <footer>
